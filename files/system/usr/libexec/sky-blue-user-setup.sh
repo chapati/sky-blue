f7179f34ba2a758087f0f6ec08abe36c91a440bb
chapati <chapati.th@gmail.com>
2026-01-03 10:34:06 +0100

    firefox

--- files/system/usr/libexec/sky-blue-user-setup.sh
+++ files/system/usr/libexec/sky-blue-user-setup.sh
@@ -4,3 +4,6 @@ if [ "${EUID:-$(id -u)}" -lt 1000 ]; then
   echo "SkyBlue user setup ran as unsupported user. Exiting."
   exit 1
 fi
+
+flatpak uninstall --system --delete-data --app org.mozilla.firefox
+flatpak uninstall --system --unused
